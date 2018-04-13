2bd3d57e1eeac619a274050e72c9fd51b4524bf2
Andrei Vaniukov <aovnkv@yahoo.com>
2018-04-13 16:33:23 +0400

    add animation when two cards are dismatched

--- js/app.js
+++ js/app.js
@@ -22,13 +22,6 @@ makeDeck(makeCardsArray());
 addClickListeners();
 starRating();
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
-
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
@@ -43,18 +36,6 @@ function shuffle(array) {
 
     return array;
 }
-
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
 
 function makeCardsArray() {
 	let i = 0;
@@ -91,15 +72,17 @@ function addClickListeners() {
 }
 
 // rebuilding the deck with new cards when clicking restart
-restartBtn.addEventListener('click', function () {
-	makeDeck(makeCardsArray());
-	addClickListeners();
-	resetStars();
-	stopTimer();
-	timerelement.innerHTML = 0;
-	oclArr = [];
-	count = 0;
-	movesBtn.innerHTML = 0;
+restartBtn.addEventListener('click', () => {
+	setTimeout(() => {
+		makeDeck(makeCardsArray());
+		addClickListeners();
+		resetStars();
+		stopTimer();
+		timerelement.innerHTML = 0;
+		oclArr = [];
+		count = 0;
+		movesBtn.innerHTML = 0;
+	}, 300);
 });
 
 // display the card's symbol and other actions
@@ -155,7 +138,9 @@ function clicksCount () {
 
 function cardsMatch (event) {
 	if (!(event.target.innerHTML === prevCard.innerHTML)) {
-		setTimeout(function () {
+		setTimeout(() => {
+			prevCard.classList.remove('no-matched');
+			event.target.classList.remove('no-matched');
 			prevCard.classList.toggle('show');
 			event.target.classList.toggle('show');
 			prevCard.classList.toggle('open');
@@ -166,6 +151,10 @@ function cardsMatch (event) {
 			oclArr.pop();
 			return prevCard = "";
 		}, 400);
+		setTimeout(() => {
+			prevCard.classList.add('no-matched');
+			event.target.classList.add('no-matched');
+		}, 100)
 	} else {
 		prevCard.classList.add('match');
 		event.target.classList.add('match');
